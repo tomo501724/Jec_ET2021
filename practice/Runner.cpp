a98729ce0b185be644cc441d322f88e70d89fefa
h0tcat <h0tnori@vivaldi.net>
2021-05-27 19:55:41 +0900

    first commit

---
+++ practice/Runner.cpp
@@ -0,0 +1,47 @@
+#include "Runner.h"
+
+Runner::Runner(
+    ev3api::Motor* leftMotor
+    ,ev3api::Motor* rightMotor
+    ,int speed){
+
+        this->leftMotor=leftMotor;
+        this->rightMotor=rightMotor;
+
+        this->setSpeed(speed);
+
+}
+
+void Runner::forward(){
+    this->leftMotor->setPWM(this->speed);
+    this->rightMotor->setPWM(this->speed);
+}
+void Runner::back(){
+    this->leftMotor->setPWM(-this->speed);
+    this->rightMotor->setPWM(-this->speed);
+
+}
+void Runner::turnLeft(){
+    this->leftMotor->stop();
+    this->rightMotor->setPWM(this->speed);
+}
+void Runner::turnRight(){
+    this->leftMotor->setPWM(this->speed);
+    this->rightMotor->stop();
+}
+void Runner::stop(){
+    this->leftMotor->stop();
+    this->rightMotor->stop();
+}
+
+void Runner::setSpeed(int speed){
+    if(speed<0){
+        speed=0;
+    }else if(speed>ev3api::Motor::PWM_MAX){
+        speed=ev3api::Motor::PWM_MAX;
+    }
+    this->speed=speed;
+}
+int Runner::getSpeed(){
+    return this->speed;
+}
