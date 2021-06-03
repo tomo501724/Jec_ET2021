f0ac64a8b00d35c52d6e8b37d1d661a8a1193f8b
h0tcat <h0tnori@vivaldi.net>
2021-06-03 21:50:43 +0900

    読みやすくするために自動フォーマットを適用

--- unit/Walker.h
+++ unit/Walker.h
@@ -3,20 +3,20 @@
 
 #include "Motor.h"
 
-class Walker{
+class Walker
+{
 
-    private:
-        ev3api::Motor& leftWheel,rightWheel;
-        int forward,turn;
-    public:
-        static const int LOW,NORMAL,HIGH,RIGHT,LEFT;
-        
-        Walker(
-            ev3api::Motor& leftWheel,
-            ev3api::Motor& rightWheel);
-        void inti();
-        void run();
-        void setCommand(int forward, int turn);
+private:
+    ev3api::Motor &leftWheel, rightWheel;
+    int forward, turn;
 
+public:
+    static const int LOW, NORMAL, HIGH, RIGHT, LEFT;
 
+    Walker(
+        ev3api::Motor &leftWheel,
+        ev3api::Motor &rightWheel);
+    void inti();
+    void run();
+    void setCommand(int forward, int turn);
 };
