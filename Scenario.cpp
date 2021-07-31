c6359e26f7a4b6e6cccd124b9185191504d200f3
DESKTOP-N5254LI\takita <mochigome66@gmail.com>
2021-07-31 17:59:55 +0900

    シナリオを変更

--- Scenario.cpp
+++ Scenario.cpp
@@ -5,34 +5,14 @@ Scenario::Scenario(Scene* scene) : mTopScene(scene), mCurrentScene(scene) {
     this->mCurrentScene = scene;
 }
 
-void Scenario::add(Scene* scene) {
-    if (mTopScene == 0) {
-        mTopScene = scene;
-    } else {
-        Scene* s = mTopScene;
-        while(s->next != top()) {
-            s = s->next;
-        }
-        s->next = scene;
-    }
-    scene->next = top();
-}
-
-Scene* Scenario::top() const {
-    return mTopScene;
-}
-
-Scene* Scenario::current() const {
-    return mCurrentScene;
-}
-
 Scene* Scenario::next() {
-    if (mCurrentScene != 0) {
-        mCurrentScene = mCurrentScene->next;
+    *mCurrentScene++;
+    if (currentSceneCommand == SceneCommands.END) {
+        return false;
     } else {
-        mCurrentScene = mTopScene;
+        return true;
     }
-    return mCurrentScene;
+
 }
 
 SceneCommands Scenario::currentSceneCommand() const {
