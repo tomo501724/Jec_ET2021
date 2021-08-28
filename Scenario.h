7ba17cb841225e2643ec9e3ed4959e3e0b78cb25
Natural_Water <21jz0121@jec.ac.jp>
2021-08-28 18:52:11 +0900

    緑丸まで記述(要値調整)

--- Scenario.h
+++ Scenario.h
@@ -7,14 +7,15 @@ class Scenario {
 private:
     Scene* mTopScene;
     Scene* mCurrentScene;
-    Scene scenes[8] = {
+    Scene scenes[9] = {
         {LINE_TRACE, 0, 6770, 100, 0.9f,  0.0f, 103.0f},
-        {GO_STRAIGHT, 0, 290, 40, 0.0f,  0.0f, 0.0f},
+        {GO_STRAIGHT, 0, 300, 40, 0.0f,  0.0f, 0.0f},
         {LINE_TRACE, 0, 790, 20, 0.9f,  0.0f, 103.0f},
-        {GO_STRAIGHT, 0, 190, 40, 0.0f,  0.0f, 0.0f},
-        {LINE_TRACE, 0, 340, 20, 0.9f,  0.0f, 103.0f},
-        {GO_STRAIGHT, 0, 50, 40, 0.0f, 0.0f, 0.0f},
-        {LINE_TRACE, 0, 460, 20, 0.9f, 0.0f, 103.0f},
+        {GO_STRAIGHT, 0, 200, 40, 0.0f,  0.0f, 0.0f},
+        {LINE_TRACE, 0, 320, 20, 0.9f,  0.0f, 103.0f},
+        {GO_STRAIGHT, 0, 100, 40, 0.0f, 0.0f, 0.0f},
+        {LINE_TRACE, 0, 570, 20, 0.9f, 0.0f, 103.0f},
+        {GO_STRAIGHT, 0, 40, 40, 0.0f, 0.0f, 0.0f},
         {END, 0, 0, 0, 0.0f, 0.0f, 0.0f}
     };
 
