c57fde72f52ab6c484627669ce74a4a0977f3473
Natural_Water <21jz0121@jec.ac.jp>
2021-08-29 15:21:13 +0900

    値調整

--- Scenario.h
+++ Scenario.h
@@ -8,8 +8,8 @@ private:
     Scene* mTopScene;
     Scene* mCurrentScene;
     Scene scenes[4] = {
-        {LINE_TRACE_UNTIL_TIME, 180U * 100000U, 67700, 100, 0.9f,  0.0f, 103.0f},
-        {LINE_TRACE_UNTIL_BLUE, 5U * 1000000U, 3000, 20, 0.9f,  0.0f, 103.0f},
+        {LINE_TRACE_UNTIL_TIME, 173U * 100000U, 67700, 100, 0.9f,  0.0f, 103.0f},
+        {GO_STRAIGHT_UNTIL_TIME, 5U * 1000000U, 3000, 20, 0.9f,  0.0f, 103.0f},
         {LINE_TRACE_UNTIL_TIME, 5U * 1000000U, 790, 20, 0.9f,  0.0f, 103.0f},
         /*{GO_STRAIGHT, 0, 200, 40, 0.0f,  0.0f, 0.0f},
         {LINE_TRACE, 0, 320, 20, 0.9f,  0.0f, 103.0f},
