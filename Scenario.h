c764c531216985db11973a0df01b17d664d75bcd
Natural_Water <21jz0121@jec.ac.jp>
2021-08-31 18:42:11 +0900

    ゴール前カーブ暴走修正(その他要微調整)

--- Scenario.h
+++ Scenario.h
@@ -8,16 +8,16 @@ private:
     Scene* mTopScene;
     Scene* mCurrentScene;
     Scene scenes[7] = {
-        {LINE_TRACE_UNTIL_TIME, 150U * 100000U, 67700, 100, 0.9f,  0.0f, 103.0f},
-        {LINE_TRACE_UNTIL_TIME, 1U * 1000000U, 67700, 90, 0.9f,  0.0f, 103.0f},
-        {LINE_TRACE_UNTIL_TIME, 150U * 10000U, 67700, 100, 0.9f,  0.0f, 103.0f},
+        {LINE_TRACE_UNTIL_TIME, 140U * 100000U, 67700, 100, 0.9f,  0.0f, 103.0f},
+        {LINE_TRACE_UNTIL_TIME, 2U * 1000000U, 67700, 70, 0.9f,  0.0f, 103.0f},
+        {LINE_TRACE_UNTIL_TIME, 180U * 10000U, 67700, 100, 0.9f,  0.0f, 103.0f},
         {GO_STRAIGHT, 0, 300, 20, 0.9f,  0.0f, 103.0f},
-        {LINE_TRACE_UNTIL_TIME, 8U * 1000000U, 790, 20, 0.9f,  0.0f, 103.0f},
-        {GO_STRAIGHT, 0, 200, 40, 0.9f,  0.0f, 103.0f},
-        /*{LINE_TRACE, 0, 320, 20, 0.9f,  0.0f, 103.0f},
-        {GO_STRAIGHT, 0, 100, 40, 0.0f, 0.0f, 0.0f},
-        {LINE_TRACE, 0, 570, 20, 0.9f, 0.0f, 103.0f},
-        {GO_STRAIGHT, 0, 40, 40, 0.0f, 0.0f, 0.0f},*/
+        {LINE_TRACE_UNTIL_TIME, 76U * 100000U, 790, 20, 0.9f,  0.0f, 103.0f},
+        {GO_STRAIGHT, 0, 200, 20, 0.9f,  0.0f, 103.0f},
+        /*{LINE_TRACE_UNTIL_TIME, 5U * 1000000U, 3200, 20, 0.9f,  0.0f, 103.0f},
+        {GO_STRAIGHT, 0, 100, 20, 0.0f, 0.0f, 0.0f},
+        {LINE_TRACE_UNTIL_TIME, 0, 570, 20, 0.9f, 0.0f, 103.0f},
+        {GO_STRAIGHT, 0, 40, 20, 0.0f, 0.0f, 0.0f},*/
         {END, 0, 0, 0, 0.0f, 0.0f, 0.0f}
     };
 
