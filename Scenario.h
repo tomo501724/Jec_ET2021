ec2840b54a985b56c43d8b955c23dd66c1133ba5
tomo_chan <tomoki.mlqll@gmail.com>
2021-09-02 19:25:59 +0900

    シナリオを一時消去

--- Scenario.h
+++ Scenario.h
@@ -8,27 +8,7 @@ private:
     Scene* mTopScene;
     Scene* mCurrentScene;
     Scene scenes[3] = {
-        /*
-        {LINE_TRACE, 0, 6630, 100, 0.9f,  0.0f, 103.0f},
-        {GO_STRAIGHT, 0, 370, 20, 0.0f,  0.0f, 0.0f},
-        {LINE_TRACE, 0, 720, 20, 0.9f,  0.0f, 103.0f},
-        {GO_STRAIGHT, 0, 250, 20, 0.0f,  0.0f, 0.0f},
-        {LINE_TRACE, 0, 360, 20, 0.9f,  0.0f, 103.0f},
-        {GO_STRAIGHT, 0, 10, 20, 0.0f, 0.0f, 0.0f},
-        */
-        //{LINE_TRACE, 17000, 6630, 100, 0.9f,  0.0f, 103.0f},
-        //{LINE_TRACE_UNTIL_TIME, 175U * 100000U, 60000, 100, 0.9f,  0.0f, 103.0f},
-        //{LINE_TRACE, 0U, 300, 100, 0.9f,  0.0f, 103.0f},
-        //{GO_STRAIGHT_UNTIL_TIME, 5U * 1000000U, 60000, 100, 0.9f,  0.0f, 103.0f},
-        //{LINE_TRACE_ON_BLUE, 0, 400, 10, 0.9f,  0.0f, 103.0f},
-        //{LINE_TRACE_UNTIL_BLUE, 0, 300, 10, 0.9f,  0.0f, 103.0f},
-        //{TURN, 0, 180, 20, 0.0f, 0.0f, 0.0f},
-        //{END, 0, 0, 0, 0.0f, 0.0f, 0.0f}
-        //{GO_STRAIGHT, 0, 370, 0, 0.9f,  0.0f, 103.0f},
-        //{LINE_TRACE_UNTIL_TIME, 175U * 100000U, 60000, 100, 0.9f,  0.0f, 103.0f},
-        {LINE_TRACE_UNTIL_BLUE, 0, 300, 0, 0.9f,  0.0f, 103.0f},
-        {LINE_TRACE_ON_BLUE, 0, 400, 10, 0.9f,  0.0f, 103.0f},
-        {END, 0, 0, 0, 0.0f, 0.0f, 0.0f}
+        /**/
     };
 
 public:
