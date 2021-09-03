e7b3630b0f38e4329424a0d060d47cbfae1f44c3
h0tcat <h0tnori@vivaldi.net>
2021-09-03 19:21:15 +0900

    パラメタの調節

--- Scenario.h
+++ Scenario.h
@@ -7,7 +7,7 @@ class Scenario {
 private:
     Scene* mTopScene;
     Scene* mCurrentScene;
-    Scene scenes[7] = {
+    Scene scenes[8] = {
         /*
         {LINE_TRACE, 0, 6630, 100, 0.9f,  0.0f, 103.0f},
         {GO_STRAIGHT, 0, 370, 20, 0.0f,  0.0f, 0.0f},
@@ -19,10 +19,11 @@ private:
         //{LINE_TRACE, 17000, 6630, 100, 0.9f,  0.0f, 103.0f},
         {LINE_TRACE_UNTIL_TIME, 175U * 100000U, 60000, 100, 0.9f,  0.0f, 103.0f},
         {GO_STRAIGHT, 0, 450, 40, 0.9f,  0.0f, 103.0f},
-        {LINE_TRACE, 0, 690, 40, 0.9f,  0.0f, 103.0f},
+        {LINE_TRACE, 0, 660, 40, 0.9f,  0.0f, 103.0f},
         {GO_STRAIGHT, 0, 420, 40, 0.9f,  0.0f, 103.0f},
         {LINE_TRACE, 0, 40, 40, 0.9f,  0.0f, 103.0f},
         {GO_STRAIGHT, 0, 420, 40, 0.9f,  0.0f, 103.0f},
+        {LINE_TRACE, 0, 40, 40, 0.9f,  0.0f, 103.0f},
         //{LINE_TRACE_ON_BLUE, 0, 400, 10, 0.9f,  0.0f, 103.0f},
         //{LINE_TRACE_UNTIL_BLUE, 0, 300, 10, 0.9f,  0.0f, 103.0f},
         {END, 0, 0, 0, 0.0f, 0.0f, 0.0f}
