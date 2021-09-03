f3811562bf643c0b84a6be3a78db8aefc513f0c0
h0tcat <h0tnori@vivaldi.net>
2021-09-03 19:27:20 +0900

    どうやら、LINE_TRACE_ON_BLUEでも以外と走れるようなので、試しに実験してみます

--- Scenario.h
+++ Scenario.h
@@ -7,7 +7,7 @@ class Scenario {
 private:
     Scene* mTopScene;
     Scene* mCurrentScene;
-    Scene scenes[8] = {
+    Scene scenes[2] = {
         /*
         {LINE_TRACE, 0, 6630, 100, 0.9f,  0.0f, 103.0f},
         {GO_STRAIGHT, 0, 370, 20, 0.0f,  0.0f, 0.0f},
@@ -17,13 +17,7 @@ private:
         {GO_STRAIGHT, 0, 10, 20, 0.0f, 0.0f, 0.0f},
         */
         //{LINE_TRACE, 17000, 6630, 100, 0.9f,  0.0f, 103.0f},
-        {LINE_TRACE_UNTIL_TIME, 175U * 100000U, 60000, 100, 0.9f,  0.0f, 103.0f},
-        {GO_STRAIGHT, 0, 450, 40, 0.9f,  0.0f, 103.0f},
-        {LINE_TRACE, 0, 660, 40, 0.9f,  0.0f, 103.0f},
-        {GO_STRAIGHT, 0, 420, 40, 0.9f,  0.0f, 103.0f},
-        {LINE_TRACE, 0, 40, 40, 0.9f,  0.0f, 103.0f},
-        {GO_STRAIGHT, 0, 420, 40, 0.9f,  0.0f, 103.0f},
-        {LINE_TRACE, 0, 40, 40, 0.9f,  0.0f, 103.0f},
+        {LINE_TRACE_ON_BLUE, 175U * 100000U, 60000, 100, 0.9f,  0.0f, 103.0f},
         //{LINE_TRACE_ON_BLUE, 0, 400, 10, 0.9f,  0.0f, 103.0f},
         //{LINE_TRACE_UNTIL_BLUE, 0, 300, 10, 0.9f,  0.0f, 103.0f},
         {END, 0, 0, 0, 0.0f, 0.0f, 0.0f}
