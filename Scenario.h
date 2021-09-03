e1e38e7a60434ea2a225d962acdbd8569f08ccec
Natural_Water <21jz0121@jec.ac.jp>
2021-09-03 20:11:46 +0900

    黄色丸後まで

--- Scenario.h
+++ Scenario.h
@@ -7,14 +7,14 @@ class Scenario {
 private:
     Scene* mTopScene;
     Scene* mCurrentScene;
-    Scene scenes[4] = {
+    Scene scenes[5] = {
         {LINE_TRACE_UNTIL_TIME, 140U * 100000U, 67700, 100, 0.9f,  0.0f, 103.0f},
         {LINE_TRACE_UNTIL_TIME, 2U * 1000000U, 67700, 70, 0.9f,  0.0f, 103.0f},
         //{LINE_TRACE_UNTIL_TIME, 1835U * 1000U, 67700, 100, 0.9f,  0.0f, 103.0f},
         //{LINE_TRACE_UNTIL_BLUE, 0, 67700, 10, 0.9f, 0.0f, 103.0f},
         {LINE_TRACE_ON_BLUE, 0, 2000, 100, 0.9f, 0.0f, 103.0f},
-        /*{GO_STRAIGHT, 0, 290, 20, 0.9f,  0.0f, 103.0f},
-        {LINE_TRACE_UNTIL_TIME, 7330U * 1000U, 790, 20, 0.9f,  0.0f, 103.0f},
+        {GO_STRAIGHT, 0, 300, 20, 0.9f,  0.0f, 103.0f},
+        /*{LINE_TRACE_UNTIL_TIME, 7330U * 1000U, 790, 20, 0.9f,  0.0f, 103.0f},
         {GO_STRAIGHT, 0, 215, 20, 0.9f,  0.0f, 103.0f},
         {LINE_TRACE_UNTIL_TIME, 3U * 1000000U, 3200, 20, 0.9f,  0.0f, 103.0f},
         {GO_STRAIGHT, 0, 120, 20, 0.0f, 0.0f, 0.0f},
